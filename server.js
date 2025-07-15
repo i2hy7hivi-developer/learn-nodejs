9da398e1df53983db15bafa6f8ab5821dfd1b86e
Rhythm Dubey <rhythm@itradicals.com>
2025-07-15 17:05:25 +0530

    Chapter 2
    Implemented the Node Server
    Created /time api

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to Node.js server!');
+    }
+
+    else if (parsedUrl.pathname === '/user' && req.method === 'GET') {
+        const name = parsedUrl.query.name || 'Guest';
+        const role = parsedUrl.query.role || 'Visitor';
+
+        const user = { name, role };
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+    }
+
+    else if (parsedUrl.pathname === '/time' && req.method === 'GET') {
+        // const name = parsedUrl.query.name || 'Guest';
+        // const role = parsedUrl.query.role || 'Visitor';
+        const date = new Date();
+
+        const datetime = { date };
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(datetime));
+    }
+
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Route not found');
+    }
+});
+
+server.listen(9000, () => {
+    console.log('Server running at http://localhost:9000');
+});
